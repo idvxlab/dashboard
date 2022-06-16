5cc31f6fbb04af9eb05877802ebf6316702bd270
skie1997 <skie1997@outlook.com>
2022-06-17 01:58:10 +0800

    Updates

---
+++ precache-manifest.8df13c07ea10d757bfacb4cc807ca9cb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "874868684a60eb620f1c5caa84a49944",
+    "url": "./index.html"
+  },
+  {
+    "revision": "7c760bfcb21beeb3a70e",
+    "url": "./static/css/2.0742c8f2.chunk.css"
+  },
+  {
+    "revision": "0acbaf51ce0af7732837",
+    "url": "./static/css/main.3b149fae.chunk.css"
+  },
+  {
+    "revision": "7c760bfcb21beeb3a70e",
+    "url": "./static/js/2.8c574872.chunk.js"
+  },
+  {
+    "revision": "0acbaf51ce0af7732837",
+    "url": "./static/js/main.7cf8230b.chunk.js"
+  },
+  {
+    "revision": "8c97409f0ee389fe75da",
+    "url": "./static/js/runtime~main.d653cc00.js"
+  }
+]);
